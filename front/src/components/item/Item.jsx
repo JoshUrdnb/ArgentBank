54f22d3667665d5b40d74ff86a776761340ba0b8
Josh Urdnb <joshua.urdanabia@gmail.com>
2025-05-08 14:07:39 +0200

    feat: créer le composant FeatureItem avec données externalisées en JSON

---
+++ front/src/components/item/Item.jsx
@@ -0,0 +1,13 @@
+import './item.scss'
+
+function Item({ icon, title, text, alt }) {
+    return (
+        <div className="feature-item">
+            <img src={icon} alt={alt} className="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
+            <p>{text}</p>
+        </div>
+    )
+}
+
+export default Item
